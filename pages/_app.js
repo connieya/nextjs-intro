0e8311966253dd1334ebff19ff04ebb3469f30ef
gunny6026 <gunny6026@naver.com>
2022-10-06 19:36:30 +0900

    feat : NavBar css

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import NavBar from "../components/NavBar";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }) {
+  return (
+    <div>
+      <NavBar />
+      <Component {...pageProps} />
+      <span>hi~~</span>
+      <style jsx global>{`
+        a {
+          color: green;
+        }
+      `}</style>
+    </div>
+  );
 }
-
-export default MyApp
